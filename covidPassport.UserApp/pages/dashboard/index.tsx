078849cbc00e1ee96cf43dcab29ad66d735cc3e7
Hristijan Gjorgjievski <hristijan_kiko123@hotmail.com>
2020-09-27 04:40:09 +1000

    Fix null exception when user has no profiles

--- covidPassport.UserApp/pages/dashboard/index.tsx
+++ covidPassport.UserApp/pages/dashboard/index.tsx
@@ -106,7 +106,9 @@ export default function Index() {
         setRows(appointmentData);
       }
 
-      loadRows(selectedProfile.id);
+      if (selectedProfile) {
+        loadRows(selectedProfile.id);
+      }
     }
 
     loadProfiles();
